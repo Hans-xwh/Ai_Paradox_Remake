a2f08268046a1dd7e9062653e08ffc231a44e8bd
Luis Calle <fc213128@gmail.com>
2025-10-26 20:08:30 -0500

    AI PARADOXINHO

--- MainForm.h
+++ MainForm.h
@@ -35,6 +35,7 @@ namespace AiParadoxRemake {
 			}
 		}
 	private: System::Windows::Forms::Button^ btn_sexo;
+	private: System::Windows::Forms::TextBox^ textBox1;
 	protected:
 
 	protected:
@@ -52,33 +53,54 @@ namespace AiParadoxRemake {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(MainForm::typeid));
 			this->btn_sexo = (gcnew System::Windows::Forms::Button());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
 			this->SuspendLayout();
 			// 
 			// btn_sexo
 			// 
-			this->btn_sexo->Location = System::Drawing::Point(102, 120);
+			this->btn_sexo->Location = System::Drawing::Point(467, 282);
 			this->btn_sexo->Name = L"btn_sexo";
-			this->btn_sexo->Size = System::Drawing::Size(75, 23);
+			this->btn_sexo->Size = System::Drawing::Size(268, 117);
 			this->btn_sexo->TabIndex = 0;
 			this->btn_sexo->Text = L"Sexo\?";
 			this->btn_sexo->UseVisualStyleBackColor = true;
 			this->btn_sexo->Click += gcnew System::EventHandler(this, &MainForm::btn_sexo_Click);
 			// 
+			// textBox1
+			// 
+			this->textBox1->BackColor = System::Drawing::SystemColors::HotTrack;
+			this->textBox1->Font = (gcnew System::Drawing::Font(L"Gill Sans Ultra Bold", 8.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->textBox1->ForeColor = System::Drawing::Color::Navy;
+			this->textBox1->Location = System::Drawing::Point(542, 210);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(145, 21);
+			this->textBox1->TabIndex = 1;
+			this->textBox1->Text = L"AI PARADOX";
+			this->textBox1->TextAlign = System::Windows::Forms::HorizontalAlignment::Center;
+			// 
 			// MainForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(284, 261);
+			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
+			this->ClientSize = System::Drawing::Size(1385, 607);
+			this->Controls->Add(this->textBox1);
 			this->Controls->Add(this->btn_sexo);
 			this->Name = L"MainForm";
 			this->Text = L"MainForm";
+			this->Load += gcnew System::EventHandler(this, &MainForm::MainForm_Load);
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
 	private: System::Void btn_sexo_Click(System::Object^ sender, System::EventArgs^ e) {
 		MessageBox::Show("Sexo.");
+	}
+	private: System::Void MainForm_Load(System::Object^ sender, System::EventArgs^ e) {
 	}
 	};
 }
